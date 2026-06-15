7c167e22e83440b438f1fbd47fbb660644b109f7
Rafael Rodrigues <rafaelrlima90@hotmail.com>
2026-06-15 03:16:23 -0300

    Implementa operacoes iniciais do sistema bancario

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import {Endereco} from './endereco';
 import {Cliente} from './cliente';
+import {ContaCorrente} from './conta-corrente';
+import {ContaPoupanca} from './conta-poupanca';
 
-const endereco = new Endereco("Rua das Flores", "São Paulo");
-const cliente = new Cliente("Rafael", endereco);
+const endereco = new Endereco("Rua das Flores", "Recife");
+const cliente = new Cliente ("Rafael", endereco); 
 
-cliente.mostrarDados();
+const contaCorrente = new ContaCorrente(1001, 1000, cliente);
+const contaPoupanca = new ContaPoupanca(2001, 5000, cliente);
+
+contaCorrente.depositar(500);
+contaPoupanca.depositar(1000);
+
+contaCorrente.sacar(300);
+
+contaPoupanca.sacar(500);
+
+contaPoupanca.sacar(1500);
