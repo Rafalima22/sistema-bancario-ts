7bd4ae5d57aadf10d8e4a435b5cf82f364b4db42
Rafael Rodrigues <rafaelrlima90@hotmail.com>
2026-06-16 06:08:50 -0300

    novo user

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,20 @@ import {ContaPoupanca} from './conta-poupanca';
 
 const endereco = new Endereco("Rua das Flores", "Recife");
 const cliente = new Cliente ("Rafael", endereco); 
+const endereco2 = new Endereco("Rua Henrique de Lucena", "Recife");
+const cliente2 = new Cliente ("Maria", endereco2);
+
+
 
 const contaCorrente = new ContaCorrente(1001, 1000, cliente);
 const contaPoupanca = new ContaPoupanca(2001, 5000, cliente);
+const contaPoupanca2 = new ContaPoupanca(2002, 3000, cliente2);
+const contaCorrente2 = new ContaCorrente(1002, 2000, cliente2);
 
 contaCorrente.depositar(500);
 contaPoupanca.depositar(1000);
+contaPoupanca2.depositar(2000);
+contaCorrente2.depositar(1500);
 
 contaCorrente.sacar(300);
 
