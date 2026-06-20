1d71a3482812a5475336cc027720d29699bfccd9
Rafael Rodrigues <rafaelrlima90@hotmail.com>
2026-06-20 05:15:53 -0300

    Array de conta

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import {Endereco} from './endereco';
-import {Cliente} from './cliente';
-import {ContaCorrente} from './conta-corrente';
-import {ContaPoupanca} from './conta-poupanca';
+import {Endereco} from './endereco.js';
+import {Cliente} from './cliente.js';
+import {ContaCorrente} from './conta-corrente.js';
+import {ContaPoupanca} from './conta-poupanca.js';
+import {Conta} from './conta.js';
 
 const endereco = new Endereco("Rua das Flores", "Recife");
 const cliente = new Cliente ("Rafael", endereco); 
@@ -10,10 +11,22 @@ const cliente2 = new Cliente ("Maria", endereco2);
 
 
 
+
 const contaCorrente = new ContaCorrente(1001, 1000, cliente);
 const contaPoupanca = new ContaPoupanca(2001, 5000, cliente);
 const contaPoupanca2 = new ContaPoupanca(2002, 3000, cliente2);
 const contaCorrente2 = new ContaCorrente(1002, 2000, cliente2);
+
+const contas: Conta[] = [
+    contaCorrente,
+    contaPoupanca,
+    contaPoupanca2,
+    contaCorrente2
+];
+
+for (const conta of contas) {
+    conta.depositar(100);
+}
 
 contaCorrente.depositar(500);
 contaPoupanca.depositar(1000);
